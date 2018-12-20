a506e801037338dd95ecbf76f99343a61260a9a6
janhajk <janhajk@gmail.com>
2018-12-20 12:57:43 +0000

    bug

--- routing.js
+++ routing.js
@@ -36,7 +36,7 @@ var basic = function(app, connection) {
             var gpg = require(__dirname + '/lib/gpgrechnung.js');
             pdfParser.on("pdfParser_dataError", errData => console.error(errData.parserError));
             pdfParser.on("pdfParser_dataReady", function(pdfData) {
-                    let data = gpg.clean(pdfData);
+                    let data = gpg.clean(pdfData, fields.headerbeginning);
                     res.send(data);
             });
             pdfParser.loadPDF(files.fileupload.path);
