5526fb201ce3b61eea07678ad216a8595da4de8b
janhajk <janhajk@gmail.com>
2018-12-20 10:15:44 +0000

    bug

--- app.js
+++ app.js
@@ -41,6 +41,10 @@ app.use(session({
     saveUninitialized: true
 }));
 
+// Configure view engine to render EJS templates.
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
